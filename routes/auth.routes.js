5615373cc6b3e0945117bc2d5ce21698b947eea4
Osvaldo Picazo <odpicazo@gmail.com>
2021-09-07 16:03:30 +0200

    added functionality to register new users

---
+++ routes/auth.routes.js
@@ -0,0 +1,34 @@
+const {Router} = require('express');
+const router = new Router();
+const User = require('../models/User.model');
+
+// get 
+router.get("/signup", (req, res) => {
+  res.render("auth/signup")
+})
+
+router.get("/userProfile", (req, res) => {
+  res.render('users/user-profile')
+})
+
+// post, use encryptation to protect password
+
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+router.post("/signup", (req, res) => {
+  console.log(req.body)
+  const {username, email, password} = req.body
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(resultFromGenSalt => bcryptjs.hash(password, resultFromGenSalt))
+    .then(resultFromHash => {
+      console.log('hashed password: ', resultFromHash)
+      return User.create({username, email, passwordHash: resultFromHash})
+      .then(resultFromUserCreate => res.redirect('/userProfile'))
+      .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
+})
+
+module.exports = router;
